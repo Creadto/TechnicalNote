586af9a30f4756316801b50dbd640b59f2daddf4
sangjin-hash <lsj9873@ajou.ac.kr>
2022-09-17 21:51:41 +0900

    CGImage<->1D-array<->CGImage 작업 중(임시)

--- Swift/ApplyingMatteEffectsToPeopleInImagesAndVideo/FacePoseSegmentation/ViewController.swift
+++ Swift/ApplyingMatteEffectsToPeopleInImagesAndVideo/FacePoseSegmentation/ViewController.swift
@@ -31,6 +31,7 @@ final class ViewController: UIViewController {
         }
     }
     
+    
     // The Metal pipeline.
     public var metalDevice: MTLDevice!
     public var metalCommandQueue: MTLCommandQueue!
@@ -71,7 +72,7 @@ final class ViewController: UIViewController {
         
         // Create a request to segment a person from an image.
         segmentationRequest = VNGeneratePersonSegmentationRequest()
-        segmentationRequest.qualityLevel = .balanced
+        segmentationRequest.qualityLevel = .accurate
         segmentationRequest.outputPixelFormat = kCVPixelFormatType_OneComponent8
     }
     
@@ -86,7 +87,6 @@ final class ViewController: UIViewController {
         // Get the pixel buffer that contains the mask image.
         guard let maskPixelBuffer =
                 segmentationRequest.results?.first?.pixelBuffer else { return }
-        
         // Process the images.
         blend(original: framePixelBuffer, mask: maskPixelBuffer)
     }
@@ -103,11 +103,21 @@ final class ViewController: UIViewController {
         // Create CIImage objects for the video frame and the segmentation mask.
         let originalImage = CIImage(cvPixelBuffer: framePixelBuffer).oriented(.right)
         var maskImage = CIImage(cvPixelBuffer: maskPixelBuffer)
-        
         // Scale the mask image to fit the bounds of the video frame.
         let scaleX = originalImage.extent.width / maskImage.extent.width
         let scaleY = originalImage.extent.height / maskImage.extent.height
         maskImage = maskImage.transformed(by: .init(scaleX: scaleX, y: scaleY))
+
+        // Convert CIImage to CGImage
+        var maskCGImage = ciContext.createCGImage(maskImage, from: maskImage.extent)!
+        // CGIamge -> 1D-array -> choose 1 channel -> CGImage
+        var filteredCGImage = convertArrayToCGImage(fromCGImage: maskCGImage)
+        // Convert CGImage to CIImage
+        var filteredCIImage = CIImage(cgImage: filteredCGImage!)
+        
+        
+//        var maskCGImage = ciContext.createCGImage(maskImage, from: maskImage.extent)!
+//        var filteredCIImage = CIImage(cgImage: maskCGImage)
         
         // Define RGB vectors for CIColorMatrix filter.
         let vectors = [
@@ -122,14 +132,129 @@ final class ViewController: UIViewController {
         
         // Blend the original, background, and mask images.
         let blendFilter = CIFilter.blendWithRedMask()
-        blendFilter.inputImage = originalImage
+////        blendFilter.inputImage = originalImage
         blendFilter.backgroundImage = backgroundImage
-        blendFilter.maskImage = maskImage
+        blendFilter.maskImage = filteredCIImage
         
         // Set the new, blended image as current.
         currentCIImage = blendFilter.outputImage?.oriented(.left)
     }
 }
+
+/**
+ *  Description : Convert CGImage to 1D-Array
+ *
+ *  @param : CGImage to convert
+ *  @return : UInt8(0~255) type 1d array(vector)
+ */
+func convertArrayToCGImage(fromCGImage imageRef: CGImage?) -> CGImage? {
+    var width = 0
+    var height = 0
+    var pixelValues: [UInt8]?
+    
+    if let imageRef = imageRef {
+        width = imageRef.width
+        height = imageRef.height
+        let bitsPerComponent = imageRef.bitsPerComponent
+        let bytesPerRow = imageRef.bytesPerRow
+        let totalBytes = height * bytesPerRow
+        let bitmapInfo = imageRef.bitmapInfo
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        var intensities = [UInt8](repeating: 0, count: totalBytes)
+        
+        let contextRef = CGContext(data: &intensities,
+                                   width: width,
+                                   height: height,
+                                   bitsPerComponent: bitsPerComponent,
+                                   bytesPerRow: bytesPerRow,
+                                   space: colorSpace,
+                                   bitmapInfo: bitmapInfo.rawValue)
+        
+        // CGImage -> 1d array로 변환하는 부분
+        contextRef?.draw(imageRef, in: CGRect(x: 0.0, y: 0.0, width: CGFloat(width), height: CGFloat(height)))
+        pixelValues = intensities
+        
+        
+
+        var filteredCGImage: CGImage?
+        if var pixelValues = pixelValues {
+            filteredCGImage = withUnsafePointer(to: &pixelValues, {
+                ptr -> CGImage? in
+                var filteredCGImage: CGImage?
+                let data = UnsafeRawPointer(ptr.pointee).assumingMemoryBound(to: UInt8.self)
+                let releaseData: CGDataProviderReleaseDataCallback = {
+                    (info: UnsafeMutableRawPointer?, data: UnsafeRawPointer, size: Int) -> () in
+                }
+                
+                if let providerRef = CGDataProvider(dataInfo: nil, data: data, size: totalBytes, releaseData: releaseData) {
+                    filteredCGImage = CGImage(width: width,
+                                              height: height,
+                                              bitsPerComponent: bitsPerComponent,
+                                              bitsPerPixel: bitsPerComponent,
+                                              bytesPerRow: bytesPerRow,
+                                              space: colorSpace,
+                                              bitmapInfo: bitmapInfo,
+                                              provider: providerRef,
+                                              decode: nil,
+                                              shouldInterpolate: false,
+                                              intent: CGColorRenderingIntent.defaultIntent)
+                }
+                
+                return filteredCGImage
+            })
+        }
+    }
+    return nil
+}
+
+    
+/**
+ *  Description : Convert 1D-Array to CGImage
+ *
+ *  @param :
+ *  @return :
+ */
+//func convertCGImage(fromPixelValues pixelValues: [UInt8]?) -> CGImage?
+//{
+//    var imageRef: CGImage?
+//    if let imageRef = imageRef {
+//        let bitsPerComponent = 8
+//        let bytesPerPixel = 1
+//        let bitsPerPixel = bytesPerPixel * bitsPerComponent
+//        let bytesPerRow = bytesPerPixel * width
+//        let totalBytes = height * bytesPerRow
+//
+//        imageRef = withUnsafePointer(to: &pixelValues, {
+//            ptr -> CGImage? in
+//            var imageRef: CGImage?
+//            let colorSpaceRef = CGColorSpaceCreateDeviceRGB()
+//            let bitmapInfo = bitmapInfo
+//            let data = UnsafeRawPointer(ptr.pointee).assumingMemoryBound(to: UInt8.self)
+//            let releaseData: CGDataProviderReleaseDataCallback = {
+//                (info: UnsafeMutableRawPointer?, data: UnsafeRawPointer, size: Int) -> () in
+//            }
+//
+//            if let providerRef = CGDataProvider(dataInfo: nil, data: data, size: totalBytes, releaseData: releaseData) {
+//                imageRef = CGImage(width: width,
+//                                   height: height,
+//                                   bitsPerComponent: bitsPerComponent,
+//                                   bitsPerPixel: bitsPerPixel,
+//                                   bytesPerRow: bytesPerRow,
+//                                   space: colorSpaceRef,
+//                                   bitmapInfo: bitmapInfo,
+//                                   provider: providerRef,
+//                                   decode: nil,
+//                                   shouldInterpolate: false,
+//                                   intent: CGColorRenderingIntent.defaultIntent)
+//            }
+//
+//            return imageRef
+//        })
+//    }
+//    return imageRef
+//}
+
 
 // MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
 
